82df20840a7e3bb591094e0342775dba456a5787
reversesh3ll <d1wepn@gmail.com>
2015-10-26 23:26:34 +1100

    Added features

--- AutoChat/AutoChat/Program.cs
+++ AutoChat/AutoChat/Program.cs
@@ -10,7 +10,6 @@ namespace AutoChat
 {
     class Program
     {
-        public static Obj_AI_Base Player = ObjectManager.Player;
         public static List<string> Congrats;
         public static List<string> Smileys;
         public static List<string> Greetings;
@@ -19,13 +18,17 @@ namespace AutoChat
         public static List<string> Sympathy;
         public static List<string> Clutch;
         public static List<string> Motivate;
+
         public static Dictionary<GameEventId, int> Events;
         public static Random random = new Random();
+
         public static float lastMessage = 0;
         public static int minDelay = 0;
         public static int maxDelay = 0;
 
-        private const string MenuName = "AutoChat";
+        
+
+        public const string MenuName = "AutoChat";
         public static Menu Settings;
 
         static void Main(string[] args)
@@ -39,10 +42,8 @@ namespace AutoChat
             setupEvents();
             setupMessages();
 
-            Chat.Print("[AutoChat Loaded]");
-
+            Console.WriteLine("[AutoChat Loaded]");
             sayGreeting();
-
             Game.OnNotify += Game_OnGameNotifyEvent;
         }
 
@@ -62,7 +63,7 @@ namespace AutoChat
             Settings.AddSeparator();
             Settings.Add("sayMessageDelayMin", new Slider("Min Message Delay", 2, 1, 3));
             Settings.Add("sayMessageDelayMax", new Slider("Max Message Delay", 4, 3, 5));
-            Settings.Add("sayMessageInterval", new Slider("Minimum Interval between messages", 240, 1, 600));
+            Settings.Add("sayMessageInterval", new Slider("Minimum Interval between messages", 180, 1, 600));
         }
 
         static void setupEvents()
@@ -71,7 +72,8 @@ namespace AutoChat
             {
                 { GameEventId.OnChampionKill, 1 },  // champion kill
                 { GameEventId.OnTurretKill, 1 }, // turret kill
-
+                { GameEventId.OnDie, 1 },
+                { GameEventId.OnKill, 1 },
                 { GameEventId.OnChampionDie, 1},  // you die
                 { GameEventId.OnChampionQuadraKill, 1 }, // quadra kill
                 { GameEventId.OnChampionPentaKill, 1 }, // penta kill
@@ -87,7 +89,7 @@ namespace AutoChat
         {
             Congrats = new List<string>
             {
-                "gj", "gj", "good job", "wp", "nice", "goodjob", "gjwp", "nice1"
+                "gj", "gj", "gj", "good job", "wp", "nice", "goodjob", "gjwp", "nice1", "nice one", "sweet"
             };
 
             Smileys = new List<string>
@@ -107,12 +109,12 @@ namespace AutoChat
 
             Apologize = new List<string>
             {
-                "damn", "sorry guys", "woops", "soz"
+                "damn", "sorry guys", "sorry", "woops", "soz", "my bad"
             };
 
             Sympathy = new List<string>
             {
-                "bl", "its ok", "next time"
+                "bl", "tough luck", "bad luck", "bummer"
             };
 
             Clutch = new List<string>
@@ -131,6 +133,15 @@ namespace AutoChat
             return collection[random.Next(collection.Count)];
         }
 
+        #region Generate Functions
+        static string generateGreeting()
+        {
+            string greeting = getRandomElement(Greetings, false);
+            greeting += " ";
+            greeting += getRandomElement(Smileys);
+            return greeting;
+        }
+
         static string generateCongratulations(string champName)
         {
             string message = getRandomElement(Congrats, false);
@@ -141,12 +152,14 @@ namespace AutoChat
             return message;
         }
 
-        static string generateGreeting()
+        static string generateSympathy(string champName)
         {
-            string greeting = getRandomElement(Greetings, false);
-            greeting += " ";
-            greeting += getRandomElement(Smileys);
-            return greeting;
+            string sympathy = getRandomElement(Sympathy, false);
+            sympathy += " ";
+            sympathy += slangName(champName);
+            sympathy += " ";
+            sympathy += getRandomElement(Smileys);
+            return sympathy;
         }
 
         static string generateApology()
@@ -157,20 +170,21 @@ namespace AutoChat
             return apology;
         }
 
-        static string generateSympathy()
-        {
-            string sympathy = getRandomElement(Sympathy, false);
-            sympathy += " ";
-            sympathy += getRandomElement(Smileys);
-            return sympathy;
-        }
-
+        
         static string generateClutch()
         {
             string clutch = getRandomElement(Clutch, false);
             clutch += " ";
             clutch += getRandomElement(Smileys);
             return clutch;
+        }
+        
+        static string generateMotivation()
+        {
+            string motivate = getRandomElement(Motivate, false);
+            motivate += " ";
+            motivate += getRandomElement(Smileys);
+            return motivate;
         }
 
         static string generateEnding()
@@ -180,11 +194,13 @@ namespace AutoChat
             ending += getRandomElement(Smileys);
             return ending;
         }
+        #endregion
 
+        #region Say Functions
         static void sayGreeting()
         {
 
-            if (Game.Time > 240) return;
+            if (Game.Time > 300) return;
 
             int minGreetingDelay = Settings["sayGreetingDelayMin"].Cast<Slider>().CurrentValue * 1000;
             int maxGreetingDelay = Settings["sayGreetingDelayMax"].Cast<Slider>().CurrentValue * 1000;
@@ -209,6 +225,15 @@ namespace AutoChat
             }
         }
 
+        static void saySympathy(string champName)
+        {
+            if (Settings["saySympathy"].Cast<CheckBox>().CurrentValue && Game.Time > lastMessage + Settings["sayMessageInterval"].Cast<Slider>().CurrentValue)
+            {
+                lastMessage = Game.Time;
+                Core.DelayAction(() => Chat.Say(generateSympathy(champName)), (random.Next(minDelay, maxDelay)));
+            }
+        }
+
         static void sayApology()
         {
             if (Settings["sayApology"].Cast<CheckBox>().CurrentValue && Game.Time > lastMessage + Settings["sayMessageInterval"].Cast<Slider>().CurrentValue)
@@ -217,22 +242,20 @@ namespace AutoChat
                 Core.DelayAction(() => Chat.Say(generateApology()), (random.Next(minDelay, maxDelay)));
             }
         }
-
-        static void saySympathy()
-        {
-            if (Settings["saySympathy"].Cast<CheckBox>().CurrentValue && Game.Time > lastMessage + Settings["sayMessageInterval"].Cast<Slider>().CurrentValue)
-            {
-                lastMessage = Game.Time;
-                Core.DelayAction(() => Chat.Say(generateSympathy()), (random.Next(minDelay, maxDelay)));
-            }
-        }
-
+        
         static void sayClutch()
         {
             if (Settings["sayCongratulate"].Cast<CheckBox>().CurrentValue)
             {
-                lastMessage = Game.Time;
                 Core.DelayAction(() => Chat.Say(generateClutch()), (random.Next(minDelay, maxDelay)));
+            }
+        }
+
+        static void sayMotivation()
+        {
+            if (Settings["sayMotivate"].Cast<CheckBox>().CurrentValue)
+            {
+                Core.DelayAction(() => Chat.Say(generateMotivation()), (random.Next(minDelay, maxDelay)));
             }
         }
 
@@ -240,143 +263,57 @@ namespace AutoChat
         {
             Core.DelayAction(() => Chat.Say("/all " + generateEnding()), (new Random(Environment.TickCount).Next(100, 1001)));
         }
+        #endregion
 
         #region Slang Names
         static string slangName(string champName)
         {
-            int chance = random.Next(0, 3);
+            int chance = random.Next(0, 5);
             if (chance == 1)
             {
                 switch (champName)
                 {
-                    case "Aatrox": return "aatrox";
-                    case "Ahri": return "ahri";
-                    case "Akali": return "akali";
                     case "Alistar": return "ali";
-                    case "Amumu": return "amumu";
-                    case "Anivia": return "anivia";
-                    case "Annie": return "annie";
-                    case "Ashe": return "ashe";
-                    case "Azir": return "azir";
-                    case "Bard": return "bard";
                     case "Blitzcrank": return "blitz";
-                    case "Brand": return "brand";
-                    case "Braum": return "braum";
                     case "Caitlyn": return "cait";
                     case "Cassiopeia": return "cass";
                     case "Cho'Gath": return "cho";
-                    case "Corki": return "corki";
-                    case "Darius": return "darius";
-                    case "Diana": return "diana";
                     case "Dr. Mundo": return "mundo";
-                    case "Draven": return "draven";
-                    case "Ekko": return "ekko";
-                    case "Elise": return "elise";
                     case "Evelynn": return "eve";
                     case "Ezreal": return "ez";
                     case "Fiddlesticks": return "fiddles";
-                    case "Fiora": return "fiora";
-                    case "Fizz": return "fizz";
-                    case "Galio": return "galio";
-                    case "Gangplank": return "gangplank";
-                    case "Garen": return "garen";
-                    case "Gnar": return "gnar";
-                    case "Gragas": return "gragas";
-                    case "Graves": return "graves";
+                    case "Gangplank": return "gp";
                     case "Hecarim": return "hec";
                     case "Heimerdinger": return "heimer";
-                    case "Irelia": return "irelia";
-                    case "Janna": return "janna";
                     case "Jarvan IV": return "j4";
-                    case "Jax": return "jax";
-                    case "Jayce": return "jayce";
-                    case "Jinx": return "jinx";
-                    case "Kalista": return "kalista";
-                    case "Karma": return "karma";
-                    case "Karthus": return "karthus";
-                    case "Kassadin": return "kassadin";
                     case "Katarina": return "kat";
-                    case "Kayle": return "kayle";
-                    case "Kennen": return "kennen";
                     case "Kha'Zix": return "khazix";
-                    case "Kindred": return "kindred";
                     case "Kog'Maw": return "kog";
                     case "LeBlanc": return "lb";
                     case "Lee Sin": return "lee";
-                    case "Leona": return "leona";
                     case "Lissandra": return "liss";
-                    case "Lucian": return "lucian";
-                    case "Lulu": return "lulu";
-                    case "Lux": return "lux";
                     case "Malphite": return "malph";
                     case "Malzahar": return "malz";
-                    case "Maokai": return "maokai";
                     case "Master Yi": return "yi";
                     case "Miss Fortune": return "mf";
                     case "Mordekaiser": return "mord";
                     case "Morgana": return "morg";
-                    case "Nami": return "nami";
-                    case "Nasus": return "nasus";
                     case "Nautilus": return "naut";
                     case "Nidalee": return "nid";
                     case "Nocturne": return "noct";
-                    case "Nunu": return "nunu";
-                    case "Olaf": return "olaf";
-                    case "Orianna": return "orianna";
-                    case "Pantheon": return "pantheon";
-                    case "Poppy": return "poppy";
-                    case "Quinn": return "quinn";
-                    case "Rammus": return "rammus";
+                    case "Orianna": return "ori";
                     case "Rek'Sai": return "reksai";
-                    case "Renekton": return "renekton";
-                    case "Rengar": return "rengar";
-                    case "Riven": return "riven";
-                    case "Rumble": return "rumble";
-                    case "Ryze": return "ryze";
                     case "Sejuani": return "sej";
-                    case "Shaco": return "shaco";
-                    case "Shen": return "shen";
-                    case "Shyvana": return "shyvana";
-                    case "Singed": return "singed";
-                    case "Sion": return "sion";
-                    case "Sivir": return "sivir";
-                    case "Skarner": return "skarner";
-                    case "Sona": return "sona";
-                    case "Soraka": return "soraka";
-                    case "Swain": return "swain";
-                    case "Syndra": return "syndra";
                     case "Tahm Kench": return "tahm";
-                    case "Talon": return "talon";
-                    case "Taric": return "taric";
-                    case "Teemo": return "teemo";
-                    case "Thresh": return "thresh";
                     case "Tristana": return "trist";
-                    case "Trundle": return "trundle";
                     case "Tryndamere": return "trynd";
                     case "Twisted Fate": return "tf";
-                    case "Twitch": return "twitch";
-                    case "Udyr": return "udyr";
-                    case "Urgot": return "urgot";
-                    case "Varus": return "varus";
-                    case "Vayne": return "vayne";
-                    case "Veigar": return "veigar";
                     case "Vel'Koz": return "velkoz";
-                    case "Vi": return "vi";
-                    case "Viktor": return "viktor";
                     case "Vladimir": return "vlad";
                     case "Volibear": return "voli";
                     case "Warwick": return "ww";
-                    case "Wukong": return "wukong";
-                    case "Xerath": return "xerath";
                     case "Xin Zhao": return "xin";
-                    case "Yasuo": return "yasuo";
-                    case "Yorick": return "yorick";
-                    case "Zac": return "zac";
-                    case "Zed": return "zed";
-                    case "Ziggs": return "ziggs";
-                    case "Zilean": return "zilean";
-                    case "Zyra": return "zyra";
-                    default: return "";
+                    default: return champName.ToLower();
                 }
             }
             return "";
@@ -390,34 +327,44 @@ namespace AutoChat
                 minDelay = Settings["sayMessageDelayMin"].Cast<Slider>().CurrentValue * 1000;
                 maxDelay = Settings["sayMessageDelayMax"].Cast<Slider>().CurrentValue * 1000;
 
-                // KILLS & TURRET KILLS
-                if (string.Equals(args.EventId.ToString(), "OnChampionKill") || string.Equals(args.EventId.ToString(), "OnTurretKill"))
+                // KILLS
+                if (args.EventId.ToString() == "OnChampionKill")
                 {
-                    Obj_AI_Base Killer = ObjectManager.GetUnitByNetworkId<Obj_AI_Base>(args.NetworkId);
-                    AIHeroClient guy = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
-                    if (Killer.IsAlly)
+                    AIHeroClient _killer = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
+                    if (_killer.IsAlly)
                     {
-                        // we will not congratulate ourselves lol :D
-                        if (!Killer.IsMe)
+                        if (!_killer.IsMe)
                         {
-                            sayCongratulations(guy.ChampionName);
+                            sayCongratulations(_killer.ChampionName);
                         }
                     }
-                    else
+                }
+
+                // DEATHS
+                if (args.EventId.ToString() == "OnChampionDie")
+                {
+                    AIHeroClient _dead = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
+                    if (_dead.IsAlly)
                     {
-                        //saySympathy();
+                        if (_dead.IsMe)
+                        {
+                            sayApology();
+                        }
+                        else
+                        {
+                            saySympathy(_dead.ChampionName);
+                        }
                     }
                 }
 
                 // DRAGON STEAL
                 if (string.Equals(args.EventId.ToString(), "OnKillDragonSteal"))
                 {
-                    Obj_AI_Base Killer = ObjectManager.GetUnitByNetworkId<Obj_AI_Base>(args.NetworkId);
+                    AIHeroClient _killer = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
 
-                    if (Killer.IsAlly)
+                    if (_killer.IsAlly)
                     {
-                        // we will not congratulate ourselves lol :D
-                        if (!Killer.IsMe)
+                        if (!_killer.IsMe)
                         {
                             sayClutch();
                         }
@@ -425,14 +372,13 @@ namespace AutoChat
                 }
 
                 // QUADRAKILL
-                if (string.Equals(args.EventId.ToString(), "OnChampionQuadraKill"))
+                if (args.EventId.ToString() == "OnChampionQuadraKill")
                 {
-                    Obj_AI_Base Killer = ObjectManager.GetUnitByNetworkId<Obj_AI_Base>(args.NetworkId);
+                    AIHeroClient _killer = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
 
-                    if (Killer.IsAlly)
+                    if (_killer.IsAlly)
                     {
-                        // we will not congratulate ourselves lol :D
-                        if (!Killer.IsMe)
+                        if (!_killer.IsMe)
                         {
                             sayClutch();
                         }
@@ -440,23 +386,32 @@ namespace AutoChat
                 }
 
                 //PENTAKILL
-                if (string.Equals(args.EventId.ToString(), "OnChampionPentaKill"))
+                if (args.EventId.ToString() == "OnChampionPentaKill")
                 {
-                    Obj_AI_Base Killer = ObjectManager.GetUnitByNetworkId<Obj_AI_Base>(args.NetworkId);
+                    AIHeroClient _killer = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
 
-                    if (Killer.IsAlly)
+                    if (_killer.IsAlly)
                     {
-                        // we will not congratulate ourselves lol :D
-                        if (!Killer.IsMe)
+                        if (!_killer.IsMe)
                         {
                             sayClutch();
                         }
                     }
                 }
 
+                //ACE
+                if (args.EventId.ToString() == "OnAce")
+                {
+                    AIHeroClient _killer = ObjectManager.GetUnitByNetworkId<AIHeroClient>(args.NetworkId);
+                    
+                    if (_killer.IsAlly)
+                    {
+                        sayMotivation();
+                    }
+                }
+
                 // END GAME
-                if (string.Equals(args.EventId.ToString(), "OnHQDie")
-                    || string.Equals(args.EventId.ToString(), "OnHQKill"))
+                if (args.EventId.ToString() == "OnHQDie" || args.EventId.ToString() == "OnHQKill")
                 {
                     saySignOff();
                 }
